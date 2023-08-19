d50e1dd2293dd5e57e78a1aacacfb1b9b6d08231
EdwinAlmanzar <EdwinAlmanzar@EDWINALMANZAR>
2023-08-19 19:36:04 -0400

    Add project files.

---
+++ ListadoEmpleados/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ListadoEmpleados.Models;
+using ListadoEmpleados.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ListadoEmpleados.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ListadoEmpContext _context;
+
+        public HomeController(ListadoEmpContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Empleado> lista = _context.Empleados.Include(c => c.oCargo).ToList();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Empleado_Detalle(int idEmpleado)
+        {
+            EmpleadoVM oEmpleadoVM = new EmpleadoVM()
+            {
+                oEmpleado = new Empleado(),
+                oListaCargo = _context.Cargos.Select(cargo => new SelectListItem()
+                {
+                    Text = cargo.Descripcion,
+                    Value = cargo.IdCargo.ToString()
+                }).ToList()
+            };
+
+            if (idEmpleado != 0)
+            {
+                oEmpleadoVM.oEmpleado = _context.Empleados.Find(idEmpleado);
+            }
+
+            return View(oEmpleadoVM);
+        }
+
+        [HttpPost]
+        public IActionResult Empleado_Detalle(EmpleadoVM oEmpleadoVM)
+        {
+            if (oEmpleadoVM.oEmpleado.IdEmpleado == 0)
+            {
+                _context.Empleados.Add(oEmpleadoVM.oEmpleado);
+            }
+            else
+            {
+                _context.Empleados.Update(oEmpleadoVM.oEmpleado);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int idEmpleado)
+        {
+            Empleado oEmpleado = new Empleado();
+
+            if (idEmpleado != 0)
+            {
+                oEmpleado = _context.Empleados.Include(r => r.oCargo).Where(r => r.IdEmpleado == idEmpleado).FirstOrDefault();
+            }
+
+            return View(oEmpleado);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(Empleado oEmpleado)
+        {
+            _context.Empleados.Remove(oEmpleado);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ ListadoEmpleados/Models/ListadoEmpContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListadoEmpleados.Models;
+
+public partial class ListadoEmpContext : DbContext
+{
+    public ListadoEmpContext()
+    {
+    }
+
+    public ListadoEmpContext(DbContextOptions<ListadoEmpContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cargo> Cargos { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cargo>(entity =>
+        {
+            entity.HasKey(e => e.IdCargo).HasName("PK__cargo__6C985625BB2DBDBC");
+
+            entity.ToTable("cargo");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__empleado__CE6D8B9E8133BACA");
+
+            entity.ToTable("empleados");
+
+            entity.Property(e => e.Correo)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.oCargo).WithMany(p => p.Empleados)
+                .HasForeignKey(d => d.IdCargo)
+                .HasConstraintName("FK__Cargo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
